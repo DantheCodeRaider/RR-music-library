5c72af87a73b07009f8ba1ddd1c5456a722a5309
Dan <dan@coderaider.tech>
2022-10-30 11:41:59 -0500

    Added useRef

--- music-library/src/App.js
+++ music-library/src/App.js
@@ -1,42 +1,42 @@
-import { useEffect, useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css';
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
 
 function App(){
-    let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
+    let searchInput = useRef('')
 
     const API_URL = 'https://itunes.apple.com/search?term='
 
     const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
-    }
-
-    useEffect(() => {
-      if(search) {
+        e.preventDefault()
         const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch(API_URL + search)
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-                setData(resData.results)
-            } else {
-                setMessage('Not Found')
-            }
-        }
-        fetchData()
+          document.title = `${term} Music`
+          const response = await fetch(API_URL + term)
+          const resData = await response.json()
+          if (resData.results.length > 0) {
+              setData(resData.results)
+          } else {
+              setMessage('Not Found')
+          }
       }
-    }, [search])
+        fetchData()
+    }
 
     return (
       <div className='App'>
         <div>
           <div className='cDiv'>
-          <SearchBar handleSearch = {handleSearch} />
+            <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+            }}>
+                <SearchBar />
+            </SearchContext.Provider>
           </div>
           <div className='cDiv'>
             {message}
